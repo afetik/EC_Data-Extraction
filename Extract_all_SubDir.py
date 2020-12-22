b7c4ae2f911feea998966140b7640c07c3895f47
afetik <afetik@gmail.com>
2020-12-22 14:56:24 +0000

    add code to extract data

---
+++ Extract_all_SubDir.py
@@ -0,0 +1,33 @@
+import tabula
+import os
+import pathlib
+#This is a local file I created which has my personal folder paths
+import My_Local_Folder_Path as lfp  
+
+#Folder path for input files for extraction
+root_dir = lfp.input_folder
+
+#Scanning for all folders and files in root directory
+for dir_, _, files in os.walk(root_dir):
+    for file_name in files:
+        rel_dir = os.path.relpath(dir_, root_dir)
+        rel_file = os.path.join(rel_dir, file_name)
+        # print(rel_file)
+        # print(rel_dir)
+        # print(file_name)
+
+        ### generating new file name
+        entrySplit = file_name.split('.pdf')
+        newFileName = entrySplit[0]
+        # print(newFileName)
+
+        #Creating new filepath using root and relative directories
+        filePath = (os.path.join(root_dir, rel_file))
+
+
+        ### output file
+        output_path = lfp.output_folder.format(newFileName)
+        tabula.convert_into(filePath, output_path, output_format="csv", pages='all', stream=True)
+
+#End of processing
+print("Done Processing Files.........")
